c2d2181a408ea71edd6cd913cc27c2c99a0f352f
David Cortes <davidcortes2407@gmail.com>
2020-07-16 16:53:57 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export default  {
+    products: [{
+        _id:'1',
+        name: 'Beer big',
+        category: 'Black',
+        image: '/images/download.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: 'Beer big',
+        category: 'Lager',
+        image: '/images/download.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'3',
+        name: 'Beer big',
+        category: 'Lager',
+        image: '/images/download.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'4',
+        name: 'Beer sota',
+        category: 'Lager',
+        image: '/images/download.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 1.5,
+        numReviews: 145
+    },
+    {
+        _id:'5',
+        name: 'Beer big',
+        category: 'Lager',
+        image: '/images/download.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    }]
+}
